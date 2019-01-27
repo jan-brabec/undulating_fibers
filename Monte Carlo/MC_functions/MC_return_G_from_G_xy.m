b9952f96841f6d5bb63dcc2cecd086e71ac78891
jan-brabec <45789777+jan-brabec@users.noreply.github.com>
2019-01-27 23:10:36 +0100

    Update MC_return_G_from_G_xy.m

--- Monte Carlo/MC_functions/MC_return_G_from_G_xy.m
+++ Monte Carlo/MC_functions/MC_return_G_from_G_xy.m
@@ -11,17 +11,12 @@ G = [];
 
 for i=2:size(G_xy,1)
     
-    
-    
     ang(i-1)  = atan((G_xy(i,2)-G_xy(i-1,2))/(G_xy(i,1)-G_xy(i-1,1)));
     length(i-1) = sqrt((G_xy(i,1)-G_xy(i-1,1))^2 + (G_xy(i,2)-G_xy(i-1,2))^2);
     
     G_incr = [length(i-1); ang(i-1)];
     G = [G G_incr];
     
-    
-
-
 end
 
 muOD = mean(ang.^2);
