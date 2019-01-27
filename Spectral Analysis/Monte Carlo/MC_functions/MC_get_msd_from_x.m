0f07dc5ff9e27b5f539aca734330976ab6ba6611
Jan Brabec <brabec.jan@protonmail.com>
2019-01-27 19:42:00 +0100

    duplicate

--- Spectral Analysis/Monte Carlo/MC_functions/MC_get_msd_from_x.m
+++
@@ -1,9 +0,0 @@
-function msd = MC_get_msd_from_x(x_act,x_init)
-% function msd = MC_get_msd_from_x(x_act,x_init)
-%
-%   Returns mean square displacement from initial and actual particle position.
-
-sd  = (x_act-x_init).^2;
-msd = mean(sd);
-
-end
--- Spectral Analysis/Monte Carlo/MC_functions/MC_get_v_from_x.m
+++
@@ -1,9 +0,0 @@
-function v = MC_get_v_from_x(x,x_prev,dt)
-%function v = MC_gen_v_from_x(x,x_prev,dt)
-%
-%   Returns particle velocities.
-
-v = (x-x_prev)/dt;
-
-end
-
