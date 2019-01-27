d3a35d376428e9ce3aad70c7caa1c0c8b4c8db5b
Jan Brabec <brabec.jan@protonmail.com>
2019-01-27 21:09:34 +0100

    uploading large files

--- Spectral Analysis/plot_f/plot_validation_spectra_and_signal_2nd_part.m
+++ Spectral Analysis/plot_f/plot_validation_spectra_and_signal_2nd_part.m
@@ -28,20 +28,22 @@ for c_case=1:4
             load res_MC_par_1e6_harmonic_3_50_1e-5.mat
     end
     
-    mc_spectra = sa_put_d_omega2zero(ft_ac_merged(:,2),mss.ac.f);
     mc_f = mss.ac.f;
+    mc_spectra = sa_put_d_omega2zero(ft_ac_merged(:,2),mc_f);
+    mc_dt = mss.ac.dt;
+    mc_phi = phi_all;
     
     switch c_case
         case 1
             load 'res_1_50.mat'
-%             title('I.')
+            %             title('I.')
             hold on
         case 2
             load 'res_1_50.mat'
             hold on
         case 3
             load 'res_3_50.mat'
-%             title('II.')
+            %             title('II.')
             hold on
         case 4
             load 'res_3_50.mat'
@@ -49,50 +51,46 @@ for c_case=1:4
             
     end
     
-        if c_case == 1 || c_case == 3
+    gs_spectrum = sa_put_d_omega2zero(ft_ac,f);
+    gs_t  = tp;
+    gs_f  = f;
+    gs_dt = dt;
+    gs_df = df;
     
-            gs_spectra = sa_put_d_omega2zero(ft_ac,f);
-            gs_f = f;
-    
-    
-            plot(mc_f,mc_spectra*1e9,'.','Markersize',12,'Color',pl_color('MC'));
-            hold on
-            plot(gs_f,gs_spectra*1e9,'Linewidth',4,'Color',pl_color('1-harm'))
-    
-            xlim([0 150])
-            ylim([0 0.2])
-    
-    
-            plot_set_2x2_in_2x2;
-            legend off;
-    
-    
-            if c_case == 1
-                xlabel('{\itf} [Hz]')
-                ylabel('D({\itf}) [μm^2/ms]')
-            end
-    
+    if c_case == 1 || c_case == 3
+        
+        gs_spectra = sa_put_d_omega2zero(ft_ac,f);
+        gs_f = f;
+        
+        
+        plot(mc_f,mc_spectra*1e9,'.','Markersize',12,'Color',pl_color('MC'));
+        hold on
+        plot(gs_f,gs_spectra*1e9,'Linewidth',4,'Color',pl_color('1-harm'))
+        
+        xlim([0 150])
+        ylim([0 0.2])
+        
+        
+        plot_set_2x2_in_2x2;
+        legend off;
+        
+        
+        if c_case == 1
+            xlabel('{\itf} [Hz]')
+            ylabel('D({\itf}) [μm^2/ms]')
         end
+        
+    end
     
     
     
     if c_case == 2 || c_case == 4
-        
-        mc_dt = mss.ac.dt;
-        mc_phi = phi_all;
         
         for i = 1:4
             [Sp_n(i),bp_n(i)] = MC_get_signal_from_phase_SDE(g_struc.a(i),mc_phi(:,i),mc_dt,g_struc.delta(i),g_struc.DELTA(i));
         end
         [bp, order] = sort(bp_n,'ascend');
         Sp = Sp_n(order);
-        
-        
-        gs_spectrum = sa_put_d_omega2zero(ft_ac,f);
-        gs_t  = tp;
-        gs_f  = f;
-        gs_dt = dt;
-        gs_df = df;
         
         grads = f_gen_grad_pulse(gs_t, g_struc.t0, g_struc.delta, g_struc.DELTA, g_struc.a);
         
@@ -112,23 +110,13 @@ for c_case=1:4
         plot(bd_n(1)*1e-9,Sd_n(1),'.','Markersize',40,'Color',pl_color('1-harm'))
         
         plot(bp_n(2)*1e-9,Sp_n(2),'s','MarkerSize',18,'MarkerEdgeColor',col, 'MarkerFaceColor',col);
-        plot(bd_n(2)*1e-9,Sd_n(2),'s','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)        
-
+        plot(bd_n(2)*1e-9,Sd_n(2),'s','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)
+        
         plot(bp_n(3)*1e-9,Sp_n(3),'p','MarkerSize',18,'MarkerEdgeColor',col, 'MarkerFaceColor',col);
-        plot(bd_n(3)*1e-9,Sd_n(3),'p','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)        
-             
+        plot(bd_n(3)*1e-9,Sd_n(3),'p','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)
+        
         plot(bp_n(4)*1e-9,Sp_n(4),'^','MarkerSize',18,'MarkerEdgeColor',col, 'MarkerFaceColor',col);
-        plot(bd_n(4)*1e-9,Sd_n(4),'^','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)        
-        
-        
-        
-%         plot(bd_n(1)*1e-9+0.2,Sd_n(1),'.','Markersize',50,'Color',pl_color('1-harm'))
-%         
-%         plot(bd_n(2)*1e-9,Sd_n(2),'s','MarkerSize',14,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)        
-% 
-%         plot(bd_n(3)*1e-9,Sd_n(3),'p','MarkerSize',14,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)        
-%              
-%         plot(bd_n(4)*1e-9,Sd_n(4),'^','MarkerSize',14,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)          
+        plot(bd_n(4)*1e-9,Sd_n(4),'^','MarkerSize',10,'MarkerEdgeColor',col2, 'MarkerFaceColor',col2)
         
         ylim([0.85 1])
         xlim([0 3])
